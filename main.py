a541a5985bbaeae05d0a96f41e87f477cfe7eeb7
Rick Ketelaar <rcketelaar00@gmail.com>
2023-06-14 15:09:56 +0200

    add button logic

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import numpy as np
 import sounddevice as sd
 from matplotlib import pyplot as plt
 from scipy.fft import rfft, rfftfreq
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+ON_BUTTON = 13
+PREV_BUTTON = 19
+NEXT_BUTTON = 26
+
+GPIO.setup(ON_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PREV_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(NEXT_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+SERVO_PINS = [1, 2, 3, 4, 5, 6]
+
+is_listening = False
+selected_index = 0
 
 DURATION = 1  # seconds
 SAMPLE_RATE = 48000  # Hz
@@ -48,10 +65,26 @@ def find_loudest_frequency(fourier, freq):
     return 0
 
 
-def main():
-    while True:
-        # input("Press enter to record.")
-        
+while True:
+    on_button_state = GPIO.input(ON_BUTTON)
+    prev_button_state = GPIO.input(PREV_BUTTON)
+    next_button_state = GPIO.input(NEXT_BUTTON)
+    
+    if on_button_state == False:
+        is_listening = not is_listening
+        time.sleep(1)
+    if prev_button_state == False:
+        if selected_index < 5:
+            selected_index += 1
+        time.sleep(1)
+    if next_button_state == False:
+        if selected_index > 0:
+            selected_index -= 1
+        time.sleep(1)
+
+    print(is_listening, selected_index)
+
+    if is_listening:
         # Record microphone input
         recording = sd.rec(int(DURATION * SAMPLE_RATE), blocking=True)
         recording = recording[:, 0]
@@ -70,9 +103,5 @@ def main():
         closest_note, closest_note_diff = find_closest_note(loudest_freq)
 
         print("Current frequency:", loudest_freq, "|",
-              "Closest note:", closest_note, "|",
-              "Difference:", closest_note_diff)
-
-
-if __name__ == '__main__':
-    main()
+            "Closest note:", closest_note, "|",
+            "Difference:", closest_note_diff)
