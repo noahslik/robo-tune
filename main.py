e07c132656f924d44249fb6b0b70e4c1a56e92c9
Rick Ketelaar <rcketelaar00@gmail.com>
2023-06-15 14:47:58 +0200

    Added early stage of servo movement to main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from scipy.fft import rfft, rfftfreq
 import RPi.GPIO as GPIO
 import time
+import pigpio
 
 GPIO.setmode(GPIO.BCM)
 
@@ -27,36 +28,24 @@ T = 1.0 / SAMPLE_RATE
 # FREQ_THRESHOLD = 1000  # MIC BOVEN KLANKKAST
 FREQ_THRESHOLD = 100  # MIC AAN NEK
 
-notes_dict = {
-    "E2": 82.41,
-    "A2": 110.00,
-    "D3": 146.83,
-    "G3": 196.00,
-    "B3": 246.94,
-    "E4": 329.63
-}
+note_frequencies = [82.41, 110.00, 146.83, 196.00, 246.94, 329.63] # E2, A2, D3, G3, B4, E4
 
 sd.default.samplerate = SAMPLE_RATE
 sd.default.channels = 1
 sd.default.dtype = "int16"
 
 
+
+def sleep(duration, get_now=time.perf_counter):
+    now = get_now()
+    end = now + duration
+    while now < end:
+        now = get_now()
+
 def plot_fourier_transform(fourier, freq):
     plt.plot(freq, fourier)
     plt.grid()
     plt.show()
-
-
-def find_closest_note(loudest_freq):
-    closest_note = None
-    closest_note_diff = None
-    for key in notes_dict:
-        diff = notes_dict[key] - loudest_freq
-        if (closest_note_diff is None) or (abs(diff) < abs(closest_note_diff)):
-            closest_note = key
-            closest_note_diff = diff
-    return closest_note, closest_note_diff
-
 
 def find_loudest_frequency(fourier, freq):
     for index, value in enumerate(fourier):
@@ -64,6 +53,37 @@ def find_loudest_frequency(fourier, freq):
             return freq[index]
     return 0
 
+def rotate_servo(selected_index, loudest_freq):
+    servo_pin = SERVO_PINS[selected_index]
+    target_frequency = note_frequencies[selected_index]
+    threshold = 5 # in Hz
+
+    frequency_diff = target_frequency - loudest_freq
+    if abs(frequency_diff) > threshold:
+
+        pwm = pigpio.pi()
+        pwm.set_mode(servo_pin, pigpio.OUTPUT)
+        pwm.set_PWM_frequency(servo_pin, 50)
+
+        if frequency_diff > 0:
+            
+            pwm.set_servo_pulsewidth(servo_pin, 1250)
+            sleep(0.5)
+            pwm.set_PWM_dutycycle(servo_pin, 0)
+            pwm.set_PWM_frequency(servo_pin, 0)
+
+        if frequency_diff < 0:
+            
+            pwm.set_servo_pulsewidth(servo_pin, 1750) 
+            sleep(0.5)
+            pwm.set_PWM_dutycycle(servo_pin, 0)
+            pwm.set_PWM_frequency(servo_pin, 0)
+
+        pwm.set_PWM_dutycycle(servo_pin, 0)
+        pwm.set_PWM_frequency(servo_pin, 0)
+        pwm.stop()
+
+    return
 
 while True:
     on_button_state = GPIO.input(ON_BUTTON)
@@ -76,10 +96,14 @@ while True:
     if prev_button_state == False:
         if selected_index < 5:
             selected_index += 1
+        if selected_index == 5:
+            selected_index = 0
         time.sleep(1)
     if next_button_state == False:
         if selected_index > 0:
             selected_index -= 1
+        if selected_index == 0:
+            selected_index = 5
         time.sleep(1)
 
     print(is_listening, selected_index)
@@ -105,3 +129,6 @@ while True:
         print("Current frequency:", loudest_freq, "|",
             "Closest note:", closest_note, "|",
             "Difference:", closest_note_diff)
+        
+        if loudest_freq > 0:
+            rotate_servo(selected_index)
